f04beef8cad7a2cd22db28465cef1c6918d9029e
vikramsingh <mr.vikram004@gmail.com>
2023-05-14 13:14:17 +0400

    unnecessary dependency removed from gradle.properties

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,25 +11,7 @@ pluginManagement {
         google()
         mavenCentral()
     }
-
-    plugins {
-        val kotlinVersion = extra["kotlin.version"] as String
-        val agpVersion = extra["agp.version"] as String
-        val composeVersion = extra["compose.version"] as String
-
-        kotlin("jvm").version(kotlinVersion)
-        kotlin("multiplatform").version(kotlinVersion)
-        kotlin("android").version(kotlinVersion)
-
-        id("com.android.application").version(agpVersion)
-        id("com.android.library").version(agpVersion)
-
-        id("org.jetbrains.compose").version(composeVersion)
-    }
 }
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
-
 
 dependencyResolutionManagement {
     repositories {
