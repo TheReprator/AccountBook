1e64c7daf7d4b12fc98d4b22bb28c1a0874d0d09
vikramsingh <mr.vikram004@gmail.com>
2023-05-24 23:16:02 +0400

    network detect for ios done, but test pending

--- shared/src/iosMain/kotlin/dev.reprator.common/connectivity/NetworkDetectorImpl.kt
+++ shared/src/iosMain/kotlin/dev.reprator.common/connectivity/NetworkDetectorImpl.kt
@@ -1,18 +1,50 @@
 package dev.reprator.common.connectivity
 
-/*
 import dev.reprator.common.util.NetworkDetector
-import platform.Foundation.*
-import platform.Network.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import platform.Network.nw_path_get_status
+import platform.Network.nw_path_monitor_cancel
+import platform.Network.nw_path_monitor_create
+import platform.Network.nw_path_monitor_set_queue
+import platform.Network.nw_path_monitor_set_update_handler
+import platform.Network.nw_path_monitor_start
+import platform.Network.nw_path_monitor_t
+import platform.Network.nw_path_status_satisfied
+import platform.Network.nw_path_status_t
+import platform.darwin.DISPATCH_QUEUE_PRIORITY_DEFAULT
+import platform.darwin.dispatch_queue_attr_make_with_qos_class
+import platform.darwin.dispatch_queue_attr_t
+import platform.darwin.dispatch_queue_create
+import platform.darwin.dispatch_queue_serial_t
+import platform.darwin.dispatch_queue_t
+import platform.posix.QOS_CLASS_UTILITY
 
-class NetworkDetectorImpl(override var isConnected: Boolean = false) : NetworkDetector {
+class NetworkDetectorImpl : NetworkDetector {
+
+    override var isConnected: MutableStateFlow<Boolean> = MutableStateFlow(false)
+
+    private var nwMonitor: nw_path_monitor_t ?= null
+    private var monitorQueue: dispatch_queue_t ?= null
 
     override fun startMonitor() {
-        val p = NWMo
-        TODO("Not yet implemented")
+        /*val attrs: dispatch_queue_attr_t = dispatch_queue_attr_make_with_qos_class(
+            dispatch_queue_serial_t.new(), QOS_CLASS_UTILITY, DISPATCH_QUEUE_PRIORITY_DEFAULT)
+        monitorQueue= dispatch_queue_create("com.example.network.monitor", attrs)*/
+        monitorQueue = dispatch_queue_create("com.example.network.monitor", null)
+
+        nwMonitor = nw_path_monitor_create()
+        nw_path_monitor_set_queue(nwMonitor, monitorQueue)
+
+        nw_path_monitor_set_update_handler(nwMonitor) { path ->
+            val status: nw_path_status_t = nw_path_get_status(path)
+            isConnected.value = nw_path_status_satisfied == status
+            println("vikram netStatus: ${isConnected.value}")
+        }
+
+        nw_path_monitor_start(nwMonitor)
     }
 
     override fun stopMonitor() {
-        TODO("Not yet implemented")
+        nw_path_monitor_cancel(nwMonitor)
     }
-}*/
+}
