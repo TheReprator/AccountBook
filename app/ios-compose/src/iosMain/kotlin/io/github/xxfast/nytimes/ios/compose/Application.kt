cf6fa9961b9bd8efd1fb3931659ed9a7422ae533
vikramsingh <mr.vikram004@gmail.com>
2023-06-12 02:08:26 +0400

    sort: ios(but app name is not reflecting, ios-compose package name change not working) Issue: web or browser content not loading, desktop working with delay

---
+++ app/ios-compose/src/iosMain/kotlin/io/github/xxfast/nytimes/ios/compose/Application.kt
@@ -0,0 +1,21 @@
+package io.github.xxfast.nytimes.ios.compose
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.window.ComposeUIViewController
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import dev.reprator.khatabook.screens.home.HomeScreen
+import io.github.xxfast.decompose.LocalComponentContext
+import platform.UIKit.UIViewController
+
+fun Main(): UIViewController = ComposeUIViewController {
+  val lifecycle = LifecycleRegistry()
+  val rootComponentContext = DefaultComponentContext(lifecycle = lifecycle)
+
+  CompositionLocalProvider(LocalComponentContext provides rootComponentContext) {
+    MaterialTheme {
+      HomeScreen()
+    }
+  }
+}
