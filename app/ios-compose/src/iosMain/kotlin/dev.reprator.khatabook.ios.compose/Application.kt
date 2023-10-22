1589601bddcc41dddea7fa51c8e45053ce29e8a2
vikramsingh <mr.vikram004@gmail.com>
2023-10-23 00:54:22 +0400

    error sorted for ios build

--- app/ios-compose/src/iosMain/kotlin/dev.reprator.khatabook.ios.compose/Application.kt
+++ app/ios-compose/src/iosMain/kotlin/dev.reprator.khatabook.ios.compose/Application.kt
@@ -3,17 +3,17 @@ package dev.reprator.khatabook.ios.compose
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.window.ComposeUIViewController
-import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import dev.reprator.khatabook.screens.home.HomeScreen
-import io.github.xxfast.decompose.LocalComponentContext
+import io.github.xxfast.decompose.router.LocalRouterContext
+import io.github.xxfast.decompose.router.RouterContext
 import platform.UIKit.UIViewController
 
 fun Main(): UIViewController = ComposeUIViewController {
   val lifecycle = LifecycleRegistry()
-  val rootComponentContext = DefaultComponentContext(lifecycle = lifecycle)
+  val rootComponentContext = RouterContext(lifecycle = lifecycle)
 
-  CompositionLocalProvider(LocalComponentContext provides rootComponentContext) {
+  CompositionLocalProvider(LocalRouterContext provides rootComponentContext) {
     MaterialTheme {
       HomeScreen()
     }
